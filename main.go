d48862c4a222f90bf9c7b63a7e6ed7d4d2ef9114
msrocka <michael.srocka@gmail.com>
2021-08-04 10:44:59 +0200

    auth

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"bytes"
 	"encoding/json"
+	"errors"
 	"fmt"
 	"io/ioutil"
 	"net/http"
@@ -30,6 +31,9 @@ func main() {
 	fmt.Println("Connected to the BuildingTransparency API")
 	fmt.Println("  with authentication token =", authKey)
 
+	// logout
+	err = logout(args)
+	check("failed to logout", err)
 }
 
 func login(args *args) (string, error) {
@@ -45,10 +49,13 @@ func login(args *args) (string, error) {
 	if err != nil {
 		return "", err
 	}
-	resp, err := http.Post(args.endpoint+"​/rest-auth​/login",
+	resp, err := http.Post(args.endpoint+"rest-auth/login",
 		"application/json", bytes.NewReader(postData))
 	if err != nil {
 		return "", err
+	}
+	if resp.StatusCode != http.StatusOK {
+		return "", errors.New("login failed: " + resp.Status)
 	}
 
 	// read key from response
@@ -64,4 +71,21 @@ func login(args *args) (string, error) {
 		return "", err
 	}
 	return key.Key, nil
+}
+
+func logout(args *args) error {
+	url := args.endpoint + "rest-auth/logout"
+	fmt.Println("Logout:")
+	fmt.Println("  POST", url)
+	req, err := http.NewRequest(http.MethodPost, url, nil)
+	if err != nil {
+		return err
+	}
+	req.Header.Set("Authorization", "Bearer "+args.authKey)
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return err
+	}
+	fmt.Println("  response: " + resp.Status)
+	return nil
 }
